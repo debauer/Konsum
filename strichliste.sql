51d92b9064f56e6313ce967a12a980a14522f60a
bunnybugsplat <33946590+bunnybugsplat@users.noreply.github.com>
2018-09-21 22:57:41 +0200

    added inital Database Structure

---
+++ strichliste.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 21, 2018 at 07:34 PM
+-- Server version: 10.1.23-MariaDB-9+deb9u1
+-- PHP Version: 7.0.30-0+deb9u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `strichliste`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Products`
+--
+
+CREATE TABLE `Products` (
+  `ProductID` int(11) NOT NULL,
+  `EAN` int(11) NOT NULL,
+  `Price` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Transaction`
+--
+
+CREATE TABLE `Transaction` (
+  `TransactionID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `TransactionAmount` float NOT NULL,
+  `TimeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `UserID` int(11) NOT NULL,
+  `NickName` text NOT NULL,
+  `KartenID` text NOT NULL,
+  `Creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Products`
+--
+ALTER TABLE `Products`
+  ADD PRIMARY KEY (`ProductID`);
+
+--
+-- Indexes for table `Transaction`
+--
+ALTER TABLE `Transaction`
+  ADD PRIMARY KEY (`TransactionID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Transaction`
+--
+ALTER TABLE `Transaction`
+  ADD CONSTRAINT `Transaction_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `User` (`UserID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
