dfde30cafd2fcd2007b29ebd41d1ea595e6dd31a
bunnybugsplat <lukas@m4us.de>
2018-09-22 04:13:37 +0200

    updated api

--- datenbank.py
+++ datenbank.py
@@ -14,16 +14,44 @@ Products = Base.classes.Products
 session = Session(engine)
 
 
-def test():
+def authenticate_pin(name="testposplsignore",pin= 12345678):
     connection = engine.connect()
-    res = connection.execute(select([User]).where(
+    res =connection.execute(select([User]).where(
                 and_(
-                    User.NickName == "Korkenzieher",
-                    User.UserPin == 1235
+                    User.NickName == name ,
+                    User.UserPin == pin
                 )
             ))
-    for row in res:
-        print (row.UserID)
+    if len(res._saved_cursor._result.rows) != 0 :
+        print("success")
+        return True
+    else:
+        print("fail")
+        return False
     connection.close()
 
-test()
+def authenticate_rfid(rfid="asdasdadasdasd"):
+    connection = engine.connect()
+    res =connection.execute(select([User]).where(
+                    User.KartenID == rfid
+            ))
+    if len(res._saved_cursor._result.rows) != 0 :
+        print("success")
+        return True
+    else:
+        print("fail")
+        return False
+    connection.close()
+
+def getBalance(UserID= 0):
+    connection = engine.connect()
+    res =connection.execute(select([Transaction]).where(
+        
+    ))
+    if len(res._saved_cursor._result.rows) != 0 :
+        print("success")
+        return True
+    else:
+        print("fail")
+        return False
+    connection.close()
